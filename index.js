f68caa9cbfbc73e306684de3ab2d76b54d57f180
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-07-20 06:43:56 +0300

    Add connection to database

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+
+const env = require('./env');
+
+mongoose
+  .connect(env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to DB...'))
+  .catch((err) => console.log('Couldnot connect to MongoDB', err));
 
 const app = express();
 
