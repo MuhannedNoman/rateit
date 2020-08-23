e39f05a60777be6ca28426d2255ceb9d4504cd61
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-23 05:59:06 +0300

    Fix user validation on rejuster

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registerd.');
+  if (!user) return res.status(400).send('User already registerd.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
   const salt = await bcrypt.genSalt(10);
