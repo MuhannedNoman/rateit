fa07e667412af585c54b5d4f90536a069a868640
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-24 06:13:44 +0300

    Get current user

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,11 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
 router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
