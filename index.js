beb249f55c8bef023ceb1f344f1b64e2f272311a
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-27 07:56:41 +0300

    Refactor validation

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const app = express();
 
 require('./startup/logging')();
 require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/database')();
+require('./startup/api')();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
