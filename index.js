b9425498ed3dd40e4fd2583fa4f107cb03bf0044
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-27 07:38:32 +0300

    Refactor express logic

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const express = require('express');
 const winston = require('winston');
 require('winston-mongodb');
 const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const error = require('./middleware/error');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 
-const env = require('./env');
+const app = express();
+require('./startup/routes')(app);
+
+require('./startup/database')();
 
 process.on('unhandledRejection', (ex) => {
   winston.error(ex.message, ex);
@@ -47,28 +43,6 @@ if (!env.JWT_SECRET_KEY) {
   console.error('FATAL ERROR: JWT secret key is not defined');
   process.exit(1);
 }
-
-mongoose
-  .connect(env.DB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('Connected to DB...'))
-  .catch((err) => console.log('Couldnot connect to MongoDB', err));
-
-const app = express();
-
-// Allow JSON parsing.
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
