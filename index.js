68828cbddd615172e6651405a1c1543751aec068
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-27 07:51:52 +0300

    Refactor logging

--- index.js
+++ index.js
@@ -1,47 +1,11 @@
-require('express-async-errors');
 const express = require('express');
-const winston = require('winston');
-require('winston-mongodb');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const env = require('./env');
-
 const app = express();
+
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/database')();
-
-process.on('unhandledRejection', (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(new winston.transports.File({ filename: `logfile.log` }));
-
-winston.add(
-  new winston.transports.File({
-    filename: `uncaughtExceptions.log`,
-    handleExceptions: true,
-  })
-);
-
-winston.add(
-  new winston.transports.File({
-    filename: `unhandledRejection.log`,
-    handleRejections: true,
-  })
-);
-
-winston.add(
-  new winston.transports.MongoDB({
-    db: env.DB_CONNECTION_STRING,
-    level: 'info',
-  })
-);
-
-if (!env.JWT_SECRET_KEY) {
-  console.error('FATAL ERROR: JWT secret key is not defined');
-  process.exit(1);
-}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ startup/logging.js
@@ -0,0 +1,29 @@
+const env = require('../env');
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports = function () {
+  winston.add(new winston.transports.File({ filename: `logfile.log` }));
+
+  winston.add(
+    new winston.transports.File({
+      filename: `uncaughtExceptions.log`,
+      handleExceptions: true,
+    })
+  );
+
+  winston.add(
+    new winston.transports.File({
+      filename: `unhandledRejection.log`,
+      handleRejections: true,
+    })
+  );
+
+  winston.add(
+    new winston.transports.MongoDB({
+      db: env.DB_CONNECTION_STRING,
+      level: 'info',
+    })
+  );
+};
