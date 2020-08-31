16dd69c06b07ca50980e0c09460adb4766a7a6bf
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-31 07:56:44 +0300

    Add user module jwt verify fn() test

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,1 +1,18 @@
-// test suit
+const { User } = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+const env = require('../../../env');
+const mongoose = require('mongoose');
+
+describe('user.generateAuthToken', () => {
+  it('should generate a vaild jwt token', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true,
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, env.JWT_SECRET_KEY);
+
+    expect(decoded).toMatchObject(payload);
+  });
+});
