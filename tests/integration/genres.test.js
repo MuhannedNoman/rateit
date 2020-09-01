b591a213fd5d21be84e19d78db7d6c9e5db5236b
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-01 09:23:49 +0300

    Insure the data of the genra API

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { Genre } = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
@@ -6,13 +7,21 @@ describe('/api/genres', () => {
     server = require('../../index');
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.remove({});
   });
   describe('GET /', () => {
     it('should return all genres', async () => {
+      await Genre.collection.insertMany([
+        { name: 'genre1' },
+        { name: 'genre2' },
+      ]);
       const res = await request(server).get('/api/genres');
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === 'genre1')).toBeTruthy();
+      expect(res.body.some((g) => g.name === 'genre2')).toBeTruthy();
     });
   });
 });
