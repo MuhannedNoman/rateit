9f695950e480d67643c4691c94d06dad96c9167a
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-07 10:16:04 +0300

    Add Test to auth middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const { User } = require('../../models/user');
+const { Genre } = require('../../models/genre');
+let server;
+
+describe('auth middleware', () => {
+  beforeEach(() => {
+    server = require('../../index');
+  });
+
+  afterEach(async () => {
+    await Genre.deleteMany({});
+    server.close();
+  });
+
+  let token;
+
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+  });
+
+  const execute = () => {
+    return request(server)
+      .post('/api/genres')
+      .set('x-auth-token', token)
+      .send({ name: 'genre1' });
+  };
+
+  it('should return 401 if no token is parovided', async () => {
+    token = '';
+    const res = await execute();
+
+    expect(res.status).toBe(401);
+  });
+
+  it('should return 400 if unvalid token is parovided', async () => {
+    token = 'a';
+    const res = await execute();
+
+    expect(res.status).toBe(400);
+  });
+
+  it('should return 200 if  token is parovided and valid', async () => {
+    const res = await execute();
+
+    expect(res.status).toBe(200);
+  });
+});
