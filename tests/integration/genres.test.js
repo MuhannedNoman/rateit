abb1d02da099ab02835db5e0be42d6b9a9b81abf
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-07 08:02:05 +0300

    Testing the genres Get endpoints

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -9,8 +9,9 @@ describe('/api/genres', () => {
 
   afterEach(async () => {
     server.close();
-    await Genre.remove({});
+    await Genre.deleteMany({});
   });
+
   describe('GET /', () => {
     it('should return all genres', async () => {
       await Genre.collection.insertMany([
@@ -22,6 +23,23 @@ describe('/api/genres', () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some((g) => g.name === 'genre1')).toBeTruthy();
       expect(res.body.some((g) => g.name === 'genre2')).toBeTruthy();
+    });
+  });
+
+  describe('/GET/:id', () => {
+    it('should return a genre with a valid id', async () => {
+      const genre = new Genre({ name: 'genre1' });
+      await genre.save();
+      const res = await request(server).get(`/api/genres/${genre._id}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
+    });
+
+    it('should return a 404 if invalid id is passed', async () => {
+      const res = await request(server).get(`/api/genres/1`);
+
+      expect(res.status).toBe(404);
     });
   });
 });
