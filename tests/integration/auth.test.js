44e5a057786cc5c0ec35e4af05262691268f8679
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-10 07:58:24 +0300

    Fix(integration/auth.test.js): typo

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -26,21 +26,21 @@ describe('auth middleware', () => {
       .send({ name: 'genre1' });
   };
 
-  it('should return 401 if no token is parovided', async () => {
+  it('should return 401 if no token is provided', async () => {
     token = '';
     const res = await execute();
 
     expect(res.status).toBe(401);
   });
 
-  it('should return 400 if unvalid token is parovided', async () => {
+  it('should return 400 if unvalid token is provided', async () => {
     token = 'a';
     const res = await execute();
 
     expect(res.status).toBe(400);
   });
 
-  it('should return 200 if  token is parovided and valid', async () => {
+  it('should return 200 if  token is provided and valid', async () => {
     const res = await execute();
 
     expect(res.status).toBe(200);
