4194334b2ad0fe598f256bf84c28afdb8aa26470
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-17 11:21:40 +0300

    Fix users route remove auth

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.get('/me', auth, async (req, res) => {
   res.send(user);
 });
 
-router.post('/', auth, async (req, res) => {
+router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
