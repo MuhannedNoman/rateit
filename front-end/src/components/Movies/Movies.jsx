6acb7c83dece7ee8b35bfde4abe5ed6af05a1377
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-26 11:03:36 +0300

    Feat(MoviesTable): extract the movies table

--- front-end/src/components/Movies/Movies.jsx
+++ front-end/src/components/Movies/Movies.jsx
@@ -4,7 +4,7 @@ import { getMovies } from '../../services/fakeMovieService';
 import { paginate } from '../../utils/Paginate';
 import ListGroup from '../ListGroup/ListGroup';
 import Pagination from '../Pagination';
-import Like from './../Like';
+import MoviesTable from './MoviesTable';
 
 const Movies = () => {
   const [allMovies, setAllMovies] = useState(getMovies());
@@ -63,39 +63,11 @@ const Movies = () => {
       </div>
       <div className="col">
         <p>{`Showing ${filtred.length} movies in the database.`}</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like liked={movie.liked} onClick={() => handleLike(movie)} />
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => handleDelete(movie)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <MoviesTable
+          onDelete={handleDelete}
+          onLike={handleLike}
+          movies={movies}
+        />
         <Pagination
           onPageChange={handlePageChange}
           items={filtred.length}
