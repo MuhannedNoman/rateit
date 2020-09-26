af6a6473d4fd950cc640798833eb269e7505aaa5
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-26 13:52:48 +0300

    Feat(TableHeader): add icon to display sort value

--- front-end/src/components/TableHeader/TableHeader.jsx
+++ front-end/src/components/TableHeader/TableHeader.jsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const TableHeader = ({ columns, sort, onSort }) => {
@@ -12,15 +13,26 @@ const TableHeader = ({ columns, sort, onSort }) => {
     onSort(newSort);
   };
 
+  const renderSortIcon = (column) => {
+    if (column.path !== sort.path) return null;
+
+    if (sort.order === 'asc')
+      return <FontAwesomeIcon icon={['fas', 'sort-up']} />;
+
+    return <FontAwesomeIcon icon={['fas', 'sort-down']} />;
+  };
+
   return (
     <thead>
       <tr>
         {columns.map((column) => (
           <th
+            role="button"
             key={column.path || column.key}
             onClick={() => raiseSort(column.path)}
           >
             {column.label}
+            {renderSortIcon(column)}
           </th>
         ))}
       </tr>
