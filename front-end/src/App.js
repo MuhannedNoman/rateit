5e3df734f931e9fa4ddc6a22a8dbbdbd16e04c2c
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-27 09:08:53 +0300

    Feat(App): add routing to the project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Customers from './components/Customers/Customers';
 import Movies from './components/Movies/Movies';
+import NotFound from './components/NotFound/NotFound';
+import Rentals from './components/Rentals/Rentals';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
-    </BrowserRouter>
+    </Router>
   );
 };
 
