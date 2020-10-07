3a7e19b79f71265278a736523959ddc69b744fcb
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-10-07 08:32:47 +0300

    Refactor(movieService): add helper fun for API url

--- front-end/src/services/movieService.js
+++ front-end/src/services/movieService.js
@@ -3,24 +3,28 @@ import { API_URL } from '../config.json';
 
 const movieEndPoint = `${API_URL}/movies`;
 
+const movieUrl = (id) => {
+  return `${movieEndPoint}/${id}`;
+};
+
 export function getMovies() {
   return http.get(movieEndPoint);
 }
 
 export function getMovie(id) {
-  return http.get(`${movieEndPoint}/${id}`);
+  return http.get(movieUrl(id));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(`${movieEndPoint}/${movie._id}`, body);
+    return http.put(movieUrl(movie._id), body);
   }
 
   return http.post(movieEndPoint, movie);
 }
 
 export function deleteMovie(id) {
-  return http.delete(`${movieEndPoint}/${id}`);
+  return http.delete(movieUrl(id));
 }
