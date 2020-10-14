01c52a1eefbf54277e6bd629f27426b56138bed5
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-10-14 09:51:21 +0300

    Feat(user): include name and email in the token

--- models/user.js
+++ models/user.js
@@ -23,7 +23,12 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
-    { _id: this._id, isAdmin: this.isAdmin },
+    {
+      _id: this._id,
+      isAdmin: this.isAdmin,
+      email: this.email,
+      name: this.email,
+    },
     env.JWT_SECRET_KEY
   );
   return token;
