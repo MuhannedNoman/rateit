10996ec4c147e215a59cdf8adcccb820bdf7cb58
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-10-18 09:53:36 +0300

    Feat(Navbar): change nav links based on user

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+  const Links = () => {
+    return user ? (
+      <>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/profile">
+            {user.name}
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/logout">
+            Logout
+          </NavLink>
+        </li>
+      </>
+    ) : (
+      <>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/login">
+            Login
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/register">
+            Register
+          </NavLink>
+        </li>
+      </>
+    );
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
@@ -35,16 +65,7 @@ const Navbar = () => {
               Rentals
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/register">
-              Register
-            </NavLink>
-          </li>
+          <Links />
         </ul>
       </div>
     </nav>
